ffc2cb097930eb2bad647f41d4f506b3bfbd09fe
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-10 00:36:32 +0800

    First test commit

---
+++ src/reducers/visibilityFilter.js
@@ -0,0 +1,12 @@
+import { types } from '../actions'
+
+const visibilityFilter = (state = types.SHOW_ALL, action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter
+    default:
+      return state
+  }
+}
+
+export default visibilityFilter
