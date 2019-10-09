88c7fc5c31ed8ac273ccf1dad06e18fde65c30f5
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-10 01:09:41 +0800

    Merge todolist container and component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
+import { toggleTodo, types } from '../actions'
 import Todo from './Todo'
 
 const TodoList = ({ todos, toggleTodo }) => (
   <ul>{todos.map(todo => <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />)}</ul>
 )
 
-export default TodoList
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case types.SHOW_ALL:
+      return todos
+    case types.SHOW_COMPLETED:
+      return todos.filter(t => t.completed)
+    case types.SHOW_ACTIVE:
+      return todos.filter(t => !t.completed)
+    default:
+      throw new Error('Unknown filter: ' + filter)
+  }
+}
+
+const mapStateToProps = state => ({
+  todos: getVisibleTodos(state.todos, state.visibilityFilter)
+})
+
+const mapDispatchToProps = dispatch => ({
+  toggleTodo: id => dispatch(toggleTodo(id))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
